33c95f9c98c7e9350c9f10fdb5fcd785ffe8e60c
jose22gonzalez <urenajosealbeto@gmail.com>
2024-05-02 15:58:34 -0400

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import Footer from './Layout/Footer/Footer'
 
 function App() {
- 
+
   return (
     <>
-    <h1 className='bg-black text-4xl text-white'>Hola mundo</h1>
+      <Footer />
     </>
   )
 }
