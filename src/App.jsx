291babeadc67aecbc3934d6718ba558282518591
jose22gonzalez <urenajosealbeto@gmail.com>
2024-05-02 17:06:49 -0400

    CS-14

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import ProductTeslaLayout from './Layout/ProductTeslaLayout/ProductTeslaLayout'
 import Footer from './Layout/Footer/Footer'
-import CarSaleLayout from './Layout/CarSale/CarSaleLayout'
 
 function App() {
 
