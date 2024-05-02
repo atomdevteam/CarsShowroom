e633f59c52bcea51202260d6c0f11124b05b0955
jose22gonzalez <urenajosealbeto@gmail.com>
2024-05-02 17:00:00 -0400

    CS-10

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import Footer from './Layout/Footer/Footer'
 import CarSaleLayout from './Layout/CarSale/CarSaleLayout'
-
 import BuyCarLayout from './Layout/BuyCarLayout/BuyCarLayout'
 function App() {
 
   return (
     <>
       <div className=''>
-        <CarSaleLayout />
+        <BuyCarLayout />
       </div>
       <Footer />
     </>
